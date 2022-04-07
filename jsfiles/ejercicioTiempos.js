bfc1c9c249c7eb0c65c88455dbffcf45ea0cdad4
Olimpo Macea <olimpo3m@gmail.com>
2022-04-07 11:33:36 -0500

    Ejercio de uso de la consola

---
+++ jsfiles/ejercicioTiempos.js
@@ -0,0 +1,23 @@
+let trabajo = "480 minutos de trabajo ";
+let estudiar = "120 minutos de estudio";
+let descansar = "15 minutos de descanso";
+let oficio = "60 minutos de oficios caseros";
+
+console.log("tareas");
+
+for (var i = 0; i < 14; i++) {
+    if (i== 0) {
+        console.group("Semana 1");
+    } 
+    console.group("dia " + (i + 1));
+    console.log(trabajo);
+    console.log(estudiar);
+    console.log(descansar);
+    console.log(oficio);
+    console.groupEnd();   
+
+    if (i == 6){
+        console.groupEnd();
+        console.group("Semana 2");
+    }
+}
