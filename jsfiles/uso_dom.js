ffd2bde6d2ecfaebcb0b5c2524c01ebe286df432
Olimpo Macea <olimpo3m@gmail.com>
2022-04-11 19:04:20 -0500

    Uso del dom modificando elementos

--- jsfiles/uso_dom.js
+++ jsfiles/uso_dom.js
@@ -4,9 +4,14 @@ let div = document.querySelector(".division");
 //document.write(div);
 // Get y Set se usan como en todos los lenguajes
 let elemento = document.querySelector(".salida");
+
+// modificar el valor del atributo type o cualquier otro atributo
+elemento.setAttribute("type", "number");
+
 valor = elemento.getAttribute("type");
 document.write(valor);
 
+// removeAttribute sirve para eliinar el atributo no el valor.
 
 
 
